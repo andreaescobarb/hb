07cc888265513d86dd496e1542c8b5ab19663026
edujguevara100 <edujguevara100@gmail.com>
2020-04-07 15:53:50 -0600

    pequeños cambios a register

--- Hospital/src/app/pages/register/register.page.ts
+++ Hospital/src/app/pages/register/register.page.ts
@@ -1,7 +1,8 @@
+import { User } from './../../servicio';
+import { UserControllerService } from './../../services/user-controller.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Usuario } from '../../usuario';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ControlContainer } from '@angular/forms';
 import { CustomValidators } from '../../custom-validators';
 
 @Component({
@@ -14,11 +15,11 @@ export class RegisterPage implements OnInit {
   private usuario = {
     'correo': "",
     'password': "",
-    'confirm':""
+    'confirm': ""
   };
   public frmSignup: FormGroup;
-
-  constructor( private router:Router, private fb: FormBuilder) { }
+  tempuser: User;
+  constructor(private controller: UserControllerService, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.usuario.correo = "";
@@ -26,31 +27,50 @@ export class RegisterPage implements OnInit {
     this.frmSignup = this.createSignupForm();
   }
 
-  userData(){
+  userData() {
     console.log(this.usuario.correo);
-    this.router.navigate(['user-data']);
+    this.getUsusario();
+    //if (this.tempuser == undefined || this.tempuser?.Correo != this.usuario.correo) {
+      let usuariofinal = {
+        "IDUser": 0,
+        "Correo": this.usuario.correo,
+        "Password": this.usuario.password,
+        "Rol": 0,
+        "Cotizaciones": 1
+      }
+      //this.controller.create(usuariofinal as User);
+      this.router.navigate(['user-data']);
+    //}
+  }
+
+  getUsusario() {
+    this.controller.getDetails(this.usuario.correo).then((response) => {
+      this.tempuser = response;
+    }, (error) => {
+      console.log("Error: " + error.statusText);
+    })
   }
   createSignupForm(): FormGroup {
     return this.fb.group(
       {
         // email is required and must be a valid email email
         email: [null, Validators.compose([
-           Validators.email,
-           Validators.required])
+          Validators.email,
+          Validators.required])
         ],
         password: [null, Validators.compose([
           Validators.required,
-          CustomValidators.patternValidator(/\d/,{hasNumber: true}),
-          CustomValidators.patternValidator(/[A-Z]/,{hasCapitalCase: true}),
-          CustomValidators.patternValidator(/[a-z]/, {hasSmallCase: true}),
+          CustomValidators.patternValidator(/\d/, { hasNumber: true }),
+          CustomValidators.patternValidator(/[A-Z]/, { hasCapitalCase: true }),
+          CustomValidators.patternValidator(/[a-z]/, { hasSmallCase: true }),
           Validators.minLength(8)
-          ]
+        ]
         )],
         confirmPassword: [null, Validators.compose([Validators.required])]
-     },
-     {
+      },
+      {
         // check whether our password and confirm password match
         validator: CustomValidators.passwordMatchValidator
-     });
+      });
   }
 }
