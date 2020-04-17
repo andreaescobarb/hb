70d579a92e094b0385352156f52a447fc60921ab
Jorge Garcia <jorgegar896@gmail.com>
2020-04-16 19:21:24 -0600

    Incorporacion de autenticacion y registro de usuario con JWT

--- Hospital/src/app/pages/menu/menu.page.ts
+++ Hospital/src/app/pages/menu/menu.page.ts
@@ -1,5 +1,6 @@
 import { Router, RouterEvent } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,19 +11,25 @@ export class MenuPage implements OnInit {
   pages = [
     {
       title: 'Inicio',
-      url: '/menu/tabs'
+      url: '/menu/tabs',
+      icon: 'home'
     },
     {
       title: 'Citas Programadas',
-      url: '/menu/cuenta'
+      url: '/menu/cuenta',
+      icon: 'calendar-sharp'
     },
     {
       title: 'Inscribir',
-      url: '/menu/inscribir-otro'
+      url: '/menu/inscribir-otro',
+      icon: 'add-sharp'
     }
   ];
   selectedPath = '';
-  constructor(private router : Router) {
+  constructor(
+    private router : Router,
+    private authenticationService: AuthenticationService
+    ) {
     this.router.events.subscribe((event:RouterEvent) => {
       if(event && event.url){
         this.selectedPath = event.url;
@@ -31,6 +38,11 @@ export class MenuPage implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  logout(){
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
